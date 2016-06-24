b02df209d1cdb8af2178678e8b9a3674f5868207
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-06-24 10:58:17 -0700

    few of my comments (in the format - "# SB: ")

--- turboworks/ABC_task.py
+++ turboworks/ABC_task.py
@@ -1,3 +1,4 @@
+# SB: I think these comments look better after the import statement
 # This FireTask is a function A*B/C=D
 # It executes this command, checks the parameter range (crude) and saves to TurboworksDB
 from fireworks.utilities.fw_utilities import explicit_serialize
@@ -6,38 +7,45 @@ import sys
 import numpy as np
 from pymongo import MongoClient
 
+#SB: re-indent the file
 
 @explicit_serialize
 class ABCtask(FireTaskBase):
-	_fw_name = "ABCTask"
+    _fw_name = "ABCTask"
 
-	def run_task(self, fw_spec):
+    def run_task(self, fw_spec):
+        """
+		# SB: Add docstrings for every function in this format, that describe what the function accomplishes, its
+		arguments (with types), and return objects/values (with types).
+		# SB: Do this for functions in other files too
+		:param fw_spec:
+        :return:
+        """
+        # Make sure we are in correct DB
+        mongo = MongoClient('localhost', 27017)
+        db = mongo.TurboWorks
+        collection = db.ABC_collection
 
-		# Make sure we are in correct DB
-		mongo = MongoClient('localhost', 27017)
-		db = mongo.TurboWorks
-		collection = db.ABC_collection
+        # Gather inputs from spec
+        A_input = fw_spec['A_input']
+        B_input = fw_spec['B_input']
+        C_input = fw_spec['C_input']
 
-		# Gather inputs from spec
-		A_input = fw_spec['A_input']
-		B_input = fw_spec['B_input']
-		C_input = fw_spec['C_input']
+        # Check to make sure params in range, this will need to be replaced with some exception system
+        if np.amax(A_input + B_input + C_input) > 100.00 or np.amin(A_input + B_input + C_input) < 1:
+            sys.exit("One or more parameters is out of range \n A,B, and C must be within 1-100")
 
-		# Check to make sure params in range, this will need to be replaced with some exception system
-		if np.amax(A_input + B_input + C_input) > 100.00 or np.amin(A_input + B_input + C_input) < 1:
-			sys.exit("One or more parameters is out of range \n A,B, and C must be within 1-100")
+        # Run black box objective algorithm (A*B/C = D)
+        D_output = np.divide(np.multiply(A_input, B_input), C_input)
+        D_output = D_output.tolist()
+        print("ABCTask ran correctly. Your D_output is: ", D_output)
 
-			# Run black box objective algorithm (A*B/C = D)
-		D_output = np.divide(np.multiply(A_input, B_input), C_input)
-		D_output = D_output.tolist()
-		print("ABCTask ran correctly. Your D_output is: ", D_output)
+        # If there is no updated info, store the values
+        if collection.find({'type': 'raw'}).count() == 0:
+            ABC_dict = {'type': 'raw', 'A_input': A_input, 'B_input': B_input,
+                        'C_input': C_input, 'D_output': D_output}
+            collection.insert_one(ABC_dict)
 
-		# If there is no updated info, store the values
-		if collection.find({'type': 'raw'}).count() == 0:
-			ABC_dict = {'type': 'raw', 'A_input': A_input, 'B_input': B_input,
-						'C_input': C_input, 'D_output': D_output}
-			collection.insert_one(ABC_dict)
-
-			# We choose not to update the spec, but store everything in the DB
-			# This isnt doing anything at the moment
-		return FWAction()
+        # We choose not to update the spec, but store everything in the DB
+        # This isnt doing anything at the moment
+        return FWAction()
