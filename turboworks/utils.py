5f760b791e425557281903515cb03bea02c3cbea
Alex Dunn <ardunn@lbl.gov>
2017-04-03 16:22:46 -0700

    remove unnecessary find_dupes func and visualize template

--- turboworks/utils.py
+++ turboworks/utils.py
@@ -46,31 +46,3 @@ def random_guess(dimensions):
 
 
     return new_input
-
-
-# TODO: I think you should remove this method (-AJ). You can simply have 'z' be a unique index in the Mongo collection which will *guarantee* you don't have duplicates. Look at ensure_index() with unique=True
-# Let me know if you think this needs to be kept (-AJ)
-def find_dupes(host='localhost', port=27017, opt_label='opt_default'):
-    """
-    For testing and development. Finds duplicate 'z' entries in the optdb.
-
-    Args:
-        host (string): host of the database
-        port (int): port of the database
-
-    Return:
-        (list) of entries which are duplicates, including duplicates of duplicates
-    """
-
-    mongo = MongoClient(host=host, port=port)
-    collection = getattr(mongo.turboworks, opt_label)
-
-    dupes = []
-    unique = []
-    for doc in collection.find():
-        if doc['z'] not in unique:
-            unique.append(doc['z'])
-        else:
-            dupes.append(doc['z'])
-
-    return dupes
