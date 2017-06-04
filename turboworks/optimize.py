09126fb740e756f5bb971e3450ee6bb2c5a495a8
Alex Dunn <ardunn@lbl.gov>
2017-06-04 12:49:59 -0700

    Deprecated random_guess
    prevent random guess from calling outside of the current search space
    by just having it randomly choose from XZ_unexplored (moved
    random_guess to parallel)

--- turboworks/optimize.py
+++ turboworks/optimize.py
@@ -279,8 +279,7 @@ class OptTask(FireTaskBase):
                         xz_new = self._postprocess(xz_onehot, xz_dims)
 
                     elif predictor == 'random_guess':
-                        x_new = random_guess(x_dims, self.dtypes)
-                        xz_new = x_new + self.collection.find({'x': x_new})['z']
+                        xz_new = random.choice(XZ_unexplored)
 
                     else:
                         if encode_categorical:
@@ -693,40 +692,4 @@ class Dtypes(object):
         self.others = d['others']
         self.discrete = self.ints + self.others
         self.all = self.numbers + self.others
-
-
-def random_guess(dimensions, dtypes=Dtypes()):
-    """
-    Returns random new inputs based on the dimensions of the search space.
-    It works with float, integer, and categorical types
-
-    Args:
-        dimensions ([tuple]): defines the dimensions of each parameter
-            example: [(1,50),(-18.939,22.435),["red", "green" , "blue", "orange"]]
-
-    Returns:
-        random_vector (list): randomly chosen next parameters in the search space
-            example: [12, 1.9383, "green"]
-    """
-
-    random_vector = []
-
-    for dimset in dimensions:
-        upper = dimset[1]
-        lower = dimset[0]
-        if type(lower) in dtypes.ints:
-            new_param = random.randint(lower, upper)
-            random_vector.append(new_param)
-        elif type(lower) in dtypes.floats:
-            new_param = random.uniform(lower, upper)
-            random_vector.append(new_param)
-        elif type(lower) in dtypes.others:
-            domain_size = len(dimset)-1
-            new_param = random.randint(0, domain_size)
-            random_vector.append(dimset[new_param])
-        else:
-            raise TypeError("The type {} is not supported by dummy opt as a categorical or "
-                            "numerical type".format(type(upper)))
-
-    return random_vector
 
--- turboworks_examples/test_parallel.py
+++ turboworks_examples/test_parallel.py
@@ -2,9 +2,9 @@
 An example of running turboworks optimizations in parallel.
 """
 
-import os
+import os, random
 from fireworks import Workflow, Firework, LaunchPad
-from turboworks.optimize import OptTask, random_guess
+from turboworks.optimize import OptTask, Dtypes
 from turboworks_examples.calculate_task import BasicCalculateTask as CalculateTask
 
 
@@ -24,6 +24,41 @@ def wf_creator(x):
                                                    duplicate_check=True,
                                                    opt_label="opt_parallel")], spec=spec)
     return Workflow([firework1])
+
+def random_guess(dimensions, dtypes=Dtypes()):
+    """
+    Returns random new inputs based on the dimensions of the search space.
+    It works with float, integer, and categorical types
+
+    Args:
+        dimensions ([tuple]): defines the dimensions of each parameter
+            example: [(1,50),(-18.939,22.435),["red", "green" , "blue", "orange"]]
+
+    Returns:
+        random_vector (list): randomly chosen next parameters in the search space
+            example: [12, 1.9383, "green"]
+    """
+
+    random_vector = []
+
+    for dimset in dimensions:
+        upper = dimset[1]
+        lower = dimset[0]
+        if type(lower) in dtypes.ints:
+            new_param = random.randint(lower, upper)
+            random_vector.append(new_param)
+        elif type(lower) in dtypes.floats:
+            new_param = random.uniform(lower, upper)
+            random_vector.append(new_param)
+        elif type(lower) in dtypes.others:
+            domain_size = len(dimset)-1
+            new_param = random.randint(0, domain_size)
+            random_vector.append(dimset[new_param])
+        else:
+            raise TypeError("The type {} is not supported by dummy opt as a categorical or "
+                            "numerical type".format(type(upper)))
+
+    return random_vector
 
 
 # try a parallel implementation of turboworks
