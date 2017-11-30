00e01aac2a0ac36990438bcc5f3d028547ea1851
ardunn <ardunn@lbl.gov>
2017-11-30 00:13:32 -0800

    Imoproved tests: they are logical now

--- rocketsled/tests.py
+++ rocketsled/tests.py
@@ -4,39 +4,105 @@ from __future__ import unicode_literals, print_function, division
 A file for testing the workflow capabilities of OptTask.
 """
 import unittest
-from rs_examples.test_basic import run_workflows as run_basic
-from rs_examples.test_categorical import run_workflows as run_categorical
-from rs_examples.test_extras import run_workflows as run_extras
-from rs_examples.test_batch import run_workflows as run_batch
-
-#todo: write better tests, these are garbage - AD
+import numpy as np
+from pymongo import MongoClient
+from fireworks import FWAction, Firework, Workflow, LaunchPad
+from fireworks.core.rocket_launcher import launch_rocket
+from fireworks.core.firework import FireTaskBase
+from fireworks.utilities.fw_utilities import explicit_serialize
+from rocketsled.optimize import OptTask
 
 __author__ = "Alexander Dunn"
 __version__ = "0.1"
 __email__ = "ardunn@lbl.gov"
 
+@explicit_serialize
+class BasicTestTask(FireTaskBase):
+    _fw_name = "BasicTestTask"
+
+    def run_task(self, fw_spec):
+        x = fw_spec['_x_opt']
+        y = np.sum(x)
+        return FWAction(update_spec={'_y_opt': y})
+
+def wf_creator_basic(x, launchpad):
+    spec = {'_x_opt': x}
+    dims = [(1, 10), (10, 20), (20, 30)]
+    bt = BasicTestTask()
+    ot = OptTask(wf_creator='rocketsled.tests.wf_creator_basic',
+                 dimensions=dims,
+                 predictor='RandomForestRegressor',
+                 predictor_kwargs={'random_state': 1},
+                 lpad=launchpad,
+                 wf_creator_args=[launchpad],
+                 opt_label='test_basic')
+    firework1 = Firework([bt, ot], spec=spec)
+    return Workflow([firework1])
+
+def wf_custom_predictor(x, launchpad):
+    spec = {'_x_opt': x}
+    dims = [(1, 10), (10, 20), (20, 30)]
+    bt = BasicTestTask()
+    ot = OptTask(wf_creator='rocketsled.tests.wf_custom_predictor',
+                 dimensions=dims,
+                 predictor='rocketsled.tests.custom_predictor',
+                 lpad=launchpad,
+                 wf_creator_args=[launchpad],
+                 opt_label='test_custom_predictor')
+    firework1 = Firework([bt, ot], spec=spec)
+    return Workflow([firework1])
+
+def custom_predictor(*args, **kwargs):
+    return [3, 12, 25]
+
+# todo: test for big workflow
+
 
 class TestWorkflows(unittest.TestCase):
-    """
-    Make sure basic optimization loops execute properly.
-    """
+    def setUp(self):
+        self.lp = LaunchPad(name='rstest')
+        self.db = MongoClient(self.lp.host, self.lp.port)
 
     def test_basic(self):
-        run_basic(test_case=True)
+        self.lp.reset(password=None, require_password=False)
+        self.lp.add_wf(wf_creator_basic([5, 11, 25], self.lp))
+        launch_rocket(self.lp)
 
-    def test_categorical(self):
-        run_categorical(test_case=True)
+        col = self.db.rstest.test_basic
+        manager = col.find_one({'y': {'$exists': 0}})
+        done = col.find_one({'y': {'$exists': 1, '$ne': 'reserved'}})
+        reserved = col.find_one({'y': 'reserved'})
 
-    def test_extras(self):
-        run_extras(test_case=True)
+        self.assertEqual(col.find({}).count(), 3)
+        self.assertEqual(manager['lock'], None)
+        self.assertEqual(manager['queue'], [])
+        self.assertEqual(done['x'], [5, 11, 25])
+        self.assertEqual(done['index'], 1)
 
-    def test_batch(self):
-        run_batch(test_case=True)
+    def test_custom_predictor(self):
+        self.lp.reset(password=None, require_password=False)
+        self.lp.add_wf(wf_custom_predictor([5, 11, 25], self.lp))
+        launch_rocket(self.lp)
+
+        col = self.db.rstest.test_custom_predictor
+        manager = col.find_one({'y': {'$exists': 0}})
+        done = col.find_one({'y': {'$exists': 1, '$ne': 'reserved'}})
+        reserved = col.find_one({'y': 'reserved'})
+
+        self.assertEqual(col.find({}).count(), 3)
+        self.assertEqual(manager['lock'], None)
+        self.assertEqual(manager['queue'], [])
+        self.assertEqual(done['x'], [5, 11, 25])
+        self.assertEqual(done['x_new'], [3, 12, 25])
+        self.assertEqual(done['index'], 1)
+        self.assertEqual(reserved['x'], [3, 12, 25])
+
+    def tearDown(self):
+        self.db.drop_database('rstest')
+
 
 def suite():
     wf_test_suite = unittest.TestSuite()
     wf_test_suite.addTest(TestWorkflows('test_basic'))
-    wf_test_suite.addTest(TestWorkflows('test_categorical'))
-    wf_test_suite.addTest(TestWorkflows('test_extras'))
-    wf_test_suite.addTest(TestWorkflows('test_batch'))
+    wf_test_suite.addTest(TestWorkflows('test_custom_predictor'))
     return wf_test_suite
