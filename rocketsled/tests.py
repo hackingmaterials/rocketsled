b20bb9013003e6f7fa26da120964aacb3069e2cb
ardunn <ardunn@lbl.gov>
2017-11-30 20:46:29 -0800

    file for specifying a db to test on

--- rocketsled/tests.py
+++ rocketsled/tests.py
@@ -2,15 +2,25 @@ from __future__ import unicode_literals, print_function, division
 
 """
 A file for testing the workflow capabilities of OptTask.
+
+Note that a local mongod instance in admin mode must be running for the tests to pass by default.
+
+WARNING: Tests reset the launchpad you specify. Specify a launchpad for testing you wouldn't mind resetting (e.g., 
+mlab.com)
+
+Modify tests_launchpad.yaml to define the db where you'd like to run the tests if you do not have access to admin
+mongod privledges on your local machine. 
+
 """
+import os
 import unittest
+import yaml
 import numpy as np
 from pymongo import MongoClient
 from fireworks import FWAction, Firework, Workflow, LaunchPad, ScriptTask
 from fireworks.core.rocket_launcher import launch_rocket, rapidfire
 from fireworks.core.firework import FireTaskBase
 from fireworks.utilities.fw_utilities import explicit_serialize
-from fw_tutorials.dynamic_wf.fibadd_task import FibonacciAdderTask
 from fw_tutorials.firetask.addition_task import AdditionTask
 from rocketsled.optimize import OptTask
 
@@ -21,6 +31,8 @@ __email__ = "ardunn@lbl.gov"
 
 # todo: test for parallel duplicates?
 # todo: test for less important params
+
+test_names = ['test_basic', 'test_custom_predictor', 'test_complex', 'test_duplicates', 'test_get_z']
 
 @explicit_serialize
 class BasicTestTask(FireTaskBase):
@@ -68,6 +80,17 @@ def wf_custom_predictor(x, launchpad):
 def wf_creator_complex(x, launchpad):
     """
     Testing a custom workflow of five fireworks with complex dependencies, and optimization in the middle.
+
+    This "complex" Workflow has the form:
+                    fw0
+                    / \
+                  fw1 fw2
+                   \  /
+                   fw3 (optimization)
+                    |
+                   fw4
+                    |
+                   fw5
     """
 
     spec = {'_x_opt': x}
@@ -110,7 +133,8 @@ def wf_creator_duplicates(x, launchpad):
 
 def wf_creator_get_z(x, launchpad):
     """
-    Testing a basic workflow with one Firework, and two FireTasks with a get_z function.
+    Testing a basic workflow with one Firework, and two FireTasks with a get_z function. Also tests that duplicate
+    checking is working with get_z.
     """
     spec = {'_x_opt': x}
     dims = [(1, 10), (10.0, 20.0), ['blue', 'green', 'red', 'orange']]
@@ -136,15 +160,18 @@ def get_z(x):
 
 class TestWorkflows(unittest.TestCase):
     def setUp(self):
-        self.lp = LaunchPad(name='rstest')
-        self.db = MongoClient(self.lp.host, self.lp.port)
+        lp_filedir = os.path.dirname(os.path.realpath(__file__))
+        lp_file = open(lp_filedir + '/tests_launchpad.yaml', 'r')
+        lp_dict = yaml.load(lp_file)
+        self.lp = LaunchPad.from_dict(lp_dict)
+        self.db = self.lp.db
 
     def test_basic(self):
         self.lp.reset(password=None, require_password=False)
         self.lp.add_wf(wf_creator_basic([5, 11, 'blue'], self.lp))
         launch_rocket(self.lp)
 
-        col = self.db.rstest.test_basic
+        col = self.db.test_basic
         manager = col.find_one({'y': {'$exists': 0}})
         done = col.find_one({'y': {'$exists': 1, '$ne': 'reserved'}})
         reserved = col.find_one({'y': 'reserved'})
@@ -160,7 +187,7 @@ class TestWorkflows(unittest.TestCase):
         self.lp.add_wf(wf_custom_predictor([5, 11, 'blue'], self.lp))
         launch_rocket(self.lp)
 
-        col = self.db.rstest.test_custom_predictor
+        col = self.db.test_custom_predictor
         manager = col.find_one({'y': {'$exists': 0}})
         done = col.find_one({'y': {'$exists': 1, '$ne': 'reserved'}})
         reserved = col.find_one({'y': 'reserved'})
@@ -179,7 +206,7 @@ class TestWorkflows(unittest.TestCase):
         for _ in range(10):
             launch_rocket(self.lp)
 
-        col = self.db.rstest.test_complex
+        col = self.db.test_complex
         loop1 = col.find({'index': 1})   # should return one doc, for the first WF
         loop2 = col.find({'index': 2})   # should return one doc, for the second WF
         reserved = col.find({'y': 'reserved'})
@@ -194,7 +221,7 @@ class TestWorkflows(unittest.TestCase):
         for _ in range(2):
             launch_rocket(self.lp)
 
-        col = self.db.rstest.test_duplicates
+        col = self.db.test_duplicates
         loop1 = col.find({'x': [5, 11, 'blue']})  # should return one doc, for the first WF
         loop2 = col.find({'x': [3, 12, 'green']})  # should return one doc, for the second WF
         reserved = col.find({'y': 'reserved'})
@@ -209,7 +236,7 @@ class TestWorkflows(unittest.TestCase):
         for _ in range(2):
             launch_rocket(self.lp)
 
-        col = self.db.rstest.test_get_z
+        col = self.db.test_get_z
         loop1 = col.find_one({'index': 1})
         loop2 = col.find_one({'index': 2})
 
@@ -220,15 +247,14 @@ class TestWorkflows(unittest.TestCase):
         self.assertEqual(loop2['z'], [9, 144.0])
 
     def tearDown(self):
-        self.db.drop_database('rstest')
+        self.lp.reset(password=None, require_password=False)
+        for tn in test_names:
+            self.db.drop_collection(tn)
 
 
 def suite():
     wf_test_suite = unittest.TestSuite()
-    wf_test_suite.addTest(TestWorkflows('test_basic'))
-    wf_test_suite.addTest(TestWorkflows('test_custom_predictor'))
-    wf_test_suite.addTest(TestWorkflows('test_complex'))
-    wf_test_suite.addTest(TestWorkflows('test_duplicates'))
-    wf_test_suite.addTest(TestWorkflows('test_get_z'))
+    for tn in test_names:
+        wf_test_suite.addTest(TestWorkflows(tn))
     return wf_test_suite
 
