154b6e53a3544c34d4324694d3090544ad93032b
ardunn <ardunn@lbl.gov>
2018-03-09 11:01:05 -0800

    Change example function for auto_setup

--- rocketsled/examples/test_auto.py
+++ rocketsled/examples/test_auto.py
@@ -6,11 +6,11 @@ An example of the using rocketsled to auto_setup a loop using a custom function.
 from rocketsled import auto_setup
 
 def myfun(x):
-    res = (float(x[0]) + float(x[1]) - float(x[2]))
+    res = float(x[0]) * float(x[1]) / float(x[2])
     return res
 
 
 if __name__ == "__main__":
-    auto_setup(myfun, dimensions=[(1, 10), (1, 10), (2, 30)],
-               wfname="my_test_wf", host='localhost',
+    auto_setup(myfun, dimensions=[(1, 100), (1, 100), (5, 65)],
+               wfname="expensive_WF", host='localhost',
                name="ROCKETSLED_EXAMPLES", port=27017, opt_label='opt_auto')
