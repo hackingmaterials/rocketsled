10ce8e6524899c6d1ffee376788a0609461a5576
ardunn <ardunn@lbl.gov>
2018-03-08 17:03:09 -0800

    skeleton for acquisition functions

---
+++ rocketsled/acquisition.py
@@ -0,0 +1,20 @@
+"""
+Acquisition functions and utilities.
+"""
+from multiprocessing import Pool, cpu_count
+
+def acquire(acq, X, Y, space, model, maximize):
+    pass
+
+def bootstrap(X, Y, space, model):
+    pass
+
+# Acquisition methods should return the acquisition values
+def ei(mu, std):
+    pass
+
+def pi(mu, std):
+    pass
+
+def lcb(mu, std):
+    pass
