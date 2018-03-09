673e006bb772a67175cbdf3f2fff2eef8e795a7e
ardunn <ardunn@lbl.gov>
2018-03-08 22:19:35 -0800

    finish auto_setup

--- rocketsled/__init__.py
+++ rocketsled/__init__.py
@@ -13,9 +13,3 @@ __author__ = "Alexander Dunn"
 __version__ = "0.1"
 __email__ = "ardunn@lbl.gov"
 
-
-__all__ = ("optdb.py",
-           "utils.py",
-           "optimize.py",
-           "references.py",
-           "visualize.py")
