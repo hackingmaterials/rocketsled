ff68c7bf3a9b9e68a7f267726e91bd1c066a2603
ardunn <ardunn@lbl.gov>
2018-03-20 15:12:06 -0700

    make sure auto does not write half-formatted files

--- rocketsled/auto.py
+++ rocketsled/auto.py
@@ -86,66 +86,71 @@ def auto_setup(func, dimensions, wfname=None, launch_ready=False, **kwargs):
         funcpath = "rocketsled.auto_sleds." + wfname + ".f"
 
         with open(filename, 'w') as f:
-            f.write("from __future__ import unicode_literals\n")
-            f.write('"""\n')
-            f.write("This is an automatically created script from auto_setup.\n"
-                    "If you are not comfortable working with FireWorks and "
-                    "PyTask, do NOT move this\nfile out this directory or "
-                    "rename it if you want to run this workflow!\n\nIf you are"
-                    " comfortable working with FireWorks and PyTask, feel f"
-                    "ree to edit\nand/or move this file to suit your needs. "
-                    "See the OptTask documentation and the\nexamples for more "
-                    "information on setting up workflow creators.\n")
-            f.write('"""\n')
-            f.write("from fireworks import PyTask, Firework, Workflow, "
-                    "LaunchPad\n")
-            f.write("from fireworks.core.rocket_launcher import rapidfire\n")
-            f.write("from rocketsled.utils import deserialize, "
-                    "random_guess\n")
-            f.write("from rocketsled import OptTask\n\n\n")
-            f.write("# This is your function, imported to rocketsled to use"
-                    " with PyTask.\n")
-            f.write("f = deserialize('" + rawfunc + "')\n\n")
-            f.write("def wf_creator(x):\n")
-            f.write("    spec = {'_x_opt':x}\n")
-            f.write("    pt = " + PyTask_as_string(funcpath) + "\n")
-            f.write("    ot = " + OptTask_as_string(**kwargs) + "\n")
-            f.write("    fw0 = Firework([pt], spec=spec, name='PyTaskFW')\n")
-            f.write("    fw1 = Firework([ot], spec=spec, "
-                    "name='RocketsledFW')\n")
-            f.write("    wf = Workflow([fw0, fw1], {fw0: [fw1], fw1: []},"
-                    " name='" + wfname + " @ ' + str(x))\n")
-            f.write("    return wf\n")
-            f.write("\n\nif __name__=='__main__': \n\n")
-            f.write("    # Make sure the launchpad below is correct, and make "
-                    "changes if necessary if\n    # it does not match the "
-                    "OptTask db ^^^:\n")
-            if all(s in kwargs for s in ['host', 'port', 'name']):
-                h = kwargs['host']
-                p = kwargs['port']
-                n = kwargs['name']
-                f.write("    lpad = LaunchPad(host='{}', port={}, "
-                        "name='{}')\n".format(h, p, n))
-            elif 'lpad' in kwargs:
-                if isinstance(kwargs['lpad'], LaunchPad):
-                    lpad = kwargs['lpad'].to_dict()
+            try:
+                f.write("from __future__ import unicode_literals\n")
+                f.write('"""\n')
+                f.write("This is an automatically created script from auto_setup.\n"
+                        "If you are not comfortable working with FireWorks and "
+                        "PyTask, do NOT move this\nfile out this directory or "
+                        "rename it if you want to run this workflow!\n\nIf you are"
+                        " comfortable working with FireWorks and PyTask, feel f"
+                        "ree to edit\nand/or move this file to suit your needs. "
+                        "See the OptTask documentation and the\nexamples for more "
+                        "information on setting up workflow creators.\n")
+                f.write('"""\n')
+                f.write("from fireworks import PyTask, Firework, Workflow, "
+                        "LaunchPad\n")
+                f.write("from fireworks.core.rocket_launcher import rapidfire\n")
+                f.write("from rocketsled.utils import deserialize, "
+                        "random_guess\n")
+                f.write("from rocketsled import OptTask\n\n\n")
+                f.write("# This is your function, imported to rocketsled to use"
+                        " with PyTask.\n")
+                f.write("f = deserialize('" + rawfunc + "')\n\n")
+                f.write("def wf_creator(x):\n")
+                f.write("    spec = {'_x_opt':x}\n")
+                f.write("    pt = " + PyTask_as_string(funcpath) + "\n")
+                f.write("    ot = " + OptTask_as_string(**kwargs) + "\n")
+                f.write("    fw0 = Firework([pt], spec=spec, name='PyTaskFW')\n")
+                f.write("    fw1 = Firework([ot], spec=spec, "
+                        "name='RocketsledFW')\n")
+                f.write("    wf = Workflow([fw0, fw1], {fw0: [fw1], fw1: []},"
+                        " name='" + wfname + " @ ' + str(x))\n")
+                f.write("    return wf\n")
+                f.write("\n\nif __name__=='__main__': \n\n")
+                f.write("    # Make sure the launchpad below is correct, and make "
+                        "changes if necessary if\n    # it does not match the "
+                        "OptTask db ^^^:\n")
+                if all(s in kwargs for s in ['host', 'port', 'name']):
+                    h = kwargs['host']
+                    p = kwargs['port']
+                    n = kwargs['name']
+                    f.write("    lpad = LaunchPad(host='{}', port={}, "
+                            "name='{}')\n".format(h, p, n))
+                elif 'lpad' in kwargs:
+                    if isinstance(kwargs['lpad'], LaunchPad):
+                        lpad = kwargs['lpad'].to_dict()
+                    else:
+                        lpad = kwargs['lpad']
+                    f.write("    lpad = LaunchPad.from_dict(" + lpad + ")\n")
                 else:
-                    lpad = kwargs['lpad']
-                f.write("    lpad = LaunchPad.from_dict(" + lpad + ")\n")
-            else:
-                f.write("    lpad = LaunchPad.auto_load()\n")
-            f.write("    # lpad.reset(password=None, require_password=False)\n")
-            f.write("\n    # Define your workflow to start...\n")
-            f.write("    wf1 = wf_creator(random_guess(" + str(dimensions) +
-                    "))\n\n")
-            f.write("    # Add it to the launchpad and launch!\n")
-            f.write("    lpad.add_wf(wf1)\n")
-            if launch_ready:
-                f.write("    rapidfire(lpad, nlaunches=5, sleep_time=0)")
-            else:
-                f.write("    # rapidfire(lpad, nlaunches=5, sleep_time=0)")
-        print("\nFile successfully created!\nFind your auto sled at "
-              "{}\n".format(filename))
+                    f.write("    lpad = LaunchPad.auto_load()\n")
+                f.write("    # lpad.reset(password=None, require_password=False)\n")
+                f.write("\n    # Define your workflow to start...\n")
+                f.write("    wf1 = wf_creator(random_guess(" + str(dimensions) +
+                        "))\n\n")
+                f.write("    # Add it to the launchpad and launch!\n")
+                f.write("    lpad.add_wf(wf1)\n")
+                if launch_ready:
+                    f.write("    rapidfire(lpad, nlaunches=5, sleep_time=0)")
+                else:
+                    f.write("    # rapidfire(lpad, nlaunches=5, sleep_time=0)")
+
+            except Exception:
+                raise
+            print("\nFile successfully created!\nFind your auto sled at "
+                  "{}\n".format(filename))
+
     else:
         raise ValueError("Please specify a callable function or a properly"
                          "formatted string location of the function")
