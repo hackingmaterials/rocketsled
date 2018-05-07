5ba10d0a0c1e93a95fd5226fe0b4fca8ea3e94cd
ardunn <ardunn@lbl.gov>
2018-05-07 15:34:11 -0700

    fixing conflict nonsense

--- rocketsled/viz.py
+++ rocketsled/viz.py
@@ -131,5 +131,5 @@ def visualize(collection, maximize=False, showbest=True, showmean=True,
 
 if __name__ == "__main__":
     from fireworks import LaunchPad
-    lpad = LaunchPad(host='localhost', port=27017, name='rf')
-    visualize(lpad.db.ei_before, maximize=False)
+    lpad = LaunchPad(host='localhost', port=27017, name='rast')
+    visualize(lpad.db.ei21, maximize=False)
