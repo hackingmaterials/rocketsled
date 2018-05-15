d84cac9b3d78a30b86cd69d4d6fc745c7061d054
ardunn <ardunn@lbl.gov>
2018-05-14 21:21:31 -0700

    Add complex multiobjective task

--- rocketsled/examples/tasks.py
+++ rocketsled/examples/tasks.py
@@ -55,3 +55,34 @@ class MultiTask6(FireTaskBase):
         y = [np.sum(x), np.prod(x), x[0] ** x[2],
              3.0 * x[1] ** 2 - 4 * x[0] - x[2], max(x), x[0] - x[1] + x[2]]
         return FWAction(update_spec={'_y_opt': y})
+
+
+@explicit_serialize
+class ComplexMultiObjTask(FireTaskBase):
+    """
+    An example of a complex, multiobjective task with competing objectives.
+
+    This problem is defined on
+    """
+    _fw_name = "CMOT"
+
+    def run_task(self, fw_spec):
+        x = fw_spec['_x_opt']
+        fin_len = x[0]
+        fin_angle = x[1]
+        fin_type = x[2]
+
+        cost = (14.1 * fin_len ** 1.847 + 12.0 + fin_angle * 100.0)/1000.0
+        drag = fin_angle ** 0.653 * float(fin_len) ** 1.2
+        failure_prob = 0.5 - fin_len/290 + (fin_angle ** 2.0)/16200
+
+        if fin_type == "shark fin":
+            cost = cost * 1.05
+            drag = drag * 1.15
+            failure_prob = failure_prob * 0.75
+        elif fin_type == "dolphin fin":
+            cost = cost * 1.6
+            drag = drag * 0.84
+            failure_prob - failure_prob * 1.75
+
+        return FWAction(update_spec={'_y_opt': [cost, drag, failure_prob], '_x_opt': x})
