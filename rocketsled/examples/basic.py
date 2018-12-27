02a828386f61204638ac371de0fcdccc516846f0
ardunn <ardunn@lbl.gov>
2018-12-26 18:51:28 -0800

    update basic example

--- rocketsled/examples/basic.py
+++ rocketsled/examples/basic.py
@@ -1,5 +1,3 @@
-from __future__ import unicode_literals, print_function, unicode_literals
-
 """
 An example of the most basic rocketsled implementation.
 This file creates and executes a workflow containing one Firework.
@@ -14,28 +12,24 @@ from fireworks import Workflow, Firework, LaunchPad
 from rocketsled import OptTask
 from rocketsled.examples.tasks import SumTask
 
-
 LPAD = LaunchPad(name='rsled')
 X_dim = [(1, 5), (1, 5), (1, 5)]
 
+
 # a workflow creator function which takes x and returns a workflow based on x
 def wf_creator(x):
-
-    spec = {'_x':x}
-
+    spec = {'_x': x}
     # SumTask writes _y field to the spec internally.
     firework1 = Firework([SumTask(),
                           OptTask(wf_creator='rocketsled.examples.basic.'
                                              'wf_creator',
                                   dimensions=X_dim,
                                   lpad=LPAD)],
-                          spec=spec)
-
+                         spec=spec)
     return Workflow([firework1])
+
 
 if __name__ == "__main__":
     LPAD.reset(password=None, require_password=False)
     LPAD.add_wf(wf_creator([5, 5, 2]))
     rapidfire(LPAD, nlaunches=10, sleep_time=0)
-
-
