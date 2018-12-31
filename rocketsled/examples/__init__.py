ce8cf221d9633ddc81599827dfb8b7cecf9028d6
ardunn <ardunn@lbl.gov>
2018-12-31 14:40:38 -0800

    update examples init

--- rocketsled/examples/__init__.py
+++ rocketsled/examples/__init__.py
@@ -1,21 +1,6 @@
-from __future__ import unicode_literals, print_function, division
-
 """
 Examples for Rocketsled.
 """
 
 __author__ = "Alexander Dunn"
-__version__ = "1.0"
 __email__ = "ardunn@lbl.gov"
-
-
-__all__ = ("tasks.py",
-           "auto.py",
-           "basic.py",
-           "batch.py",
-           "categorical.py",
-           "complex.py",
-           "extras.py",
-           "multi.py",
-           "parallel.py",
-           "tasks.py")
