0dd4af87c836f6bb6545093e10d88f8e571d2ca8
ardunn <ardunn@lbl.gov>
2018-12-31 19:09:08 -0800

    update version

--- rocketsled/__init__.py
+++ rocketsled/__init__.py
@@ -7,6 +7,6 @@ from rocketsled.control import MissionControl
 from rocketsled.task import OptTask
 
 __author__ = "Alexander Dunn"
-__version__ = "2018.12.30"
+__version__ = "2018.12.31"
 __email__ = "ardunn@lbl.gov"
 
