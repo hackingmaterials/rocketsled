2a403b49f0a1c7a37c71b88d9676088f30bc2a19
ardunn <ardunn@lbl.gov>
2018-12-31 19:13:51 -0800

    update index docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '2018.12.30',
+    VERSION: '2018.12.31',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
