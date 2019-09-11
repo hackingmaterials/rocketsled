4f3dc47a0e7c78ecb6f81c21b7ff40769b13e7cc
ardunn <ardunn@lbl.gov>
2019-09-11 14:17:35 -0700

    update tasks, setup, version

--- tasks.py
+++ tasks.py
@@ -6,18 +6,15 @@ import os
 import json
 import webbrowser
 import requests
+import datetime
 from invoke import task
+
 from rocketsled import __version__
 from monty.os import cd
-
 
 """
 Deployment file to facilitate releases.
 """
-
-__author__ = "Shyue Ping Ong, Anubhav Jain"
-__email__ = "ongsp@ucsd.edu"
-__date__ = "Sep 1, 2014"
 
 
 @task
@@ -37,26 +34,55 @@ def make_doc(ctx):
 
 
 @task
-def update_doc(ctx):
-    make_doc(ctx)
-    with cd("docs"):
-        ctx.run("git add .")
-        ctx.run("git commit -a -m \"Update to v{}\"".format(__version__))
-        ctx.run("git push")
+def open_doc(ctx):
+    pth = os.path.abspath("docs/index.html")
+    webbrowser.open("file://" + pth)
+
+
+@task
+def version_check(ctx):
+    with open("setup.py", "r") as f:
+        setup_version = None
+        for l in f.readlines():
+            if "version = " in l:
+                setup_version = l.split(" ")[-1]
+                setup_version = setup_version.replace('"', "").replace("\n", "")
+
+    if setup_version is None:
+        raise IOError("Could not parse setup.py for version.")
+
+    if __version__ == setup_version:
+        print("Setup and init versions match eachother.")
+        today = datetime.date.today().strftime("%Y.%-m.%-d")
+        if today != __version__:
+            raise ValueError(f"The version {__version__} does not match "
+                             f"the date!")
+        else:
+            print("Version matches the date.")
+    else:
+        raise ValueError(f"There is a mismatch in the date between the "
+                         f"rocketsled __init__ and the setup. Please "
+                         f"make sure they are the same."
+                         f"\n DIFF: {__version__}, {setup_version}")
+
+
+@task
+def update_changelog(ctx):
+    version_check(ctx)
+    ctx.run('github_changelog_generator hackingmaterials/rocketsled')
 
 
 @task
 def publish(ctx):
-    ctx.run("rm dist/*.*", warn=True)
+    version_check(ctx)
+    ctx.run("rm dist/*.* build/*", warn=True)
     ctx.run("python3 setup.py sdist bdist_wheel")
     ctx.run("twine upload dist/*")
 
-@task
-def update_changelog(ctx):
-    ctx.run('github_changelog_generator hackingmaterials/rocketsled')
 
 @task
 def release(ctx):
+    version_check(ctx)
     payload = {
         "tag_name": "v" + __version__,
         "target_commitish": "master",
@@ -68,10 +94,6 @@ def release(ctx):
     response = requests.post(
         "https://api.github.com/repos/hackingmaterials/rocketsled/releases",
         data=json.dumps(payload),
-        headers={"Authorization": "token " + os.environ["GITHUB_RELEASES_TOKEN"]})
+        headers={
+            "Authorization": "token " + os.environ["GITHUB_RELEASES_TOKEN"]})
     print(response.text)
-
-@task
-def open_doc(ctx):
-    pth = os.path.abspath("docs/index.html")
-    webbrowser.open("file://" + pth)
